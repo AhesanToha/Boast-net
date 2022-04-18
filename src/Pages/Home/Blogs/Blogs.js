78f032ee0c3e57b80cf18e2c74ae5c30736973cc
Toha92 <ahesan868@gmail.com>
2022-04-18 16:51:22 +0600

    blog pages fully implemented

--- src/Pages/Home/Blogs/Blogs.js
+++ src/Pages/Home/Blogs/Blogs.js
@@ -5,6 +5,7 @@ import "./Blogs.css";
 const Blogs = () => {
   return (
     <div className="blog-container container">
+      
       <div className="single-container">
         <div className="single">
           <h2>Authentication</h2>
@@ -28,6 +29,28 @@ const Blogs = () => {
           <p><CheckCircleIcon height={20}/>Authorization always takes place after authentication.</p>
           <p><CheckCircleIcon height={20}/>Authorization isn’t visible to or changeable by the user.</p>
           <p><CheckCircleIcon height={20}/>Authorization works through settings that are implemented and maintained by the organization.</p>
+        </div>
+      </div>
+
+      <div className="single-container">
+        <div className="single">
+          <h2>*Why I use firebase?</h2>
+          <p style={{margin:'0 5px', color:'#baeaf2'}}>Many characteristics distinguish Google Firebase as the go-to backend development tool for web and mobile projects. It decreases the amount of effort and time spent on development. It's also an excellent prototyping tool. Firebase is a simple, lightweight, user-friendly, and well-known platform.</p>
+          <h2>*Firebase Alternatives</h2>
+          <p><CheckCircleIcon height={20}/>Parse</p>
+          <p><CheckCircleIcon height={20}/>Back4App</p>
+          <p><CheckCircleIcon height={20}/>Kuzzle</p>
+          <p><CheckCircleIcon height={20}/>Flutter</p>
+        </div>
+      </div>
+      <div className="single-container">
+        <div className="single">
+          <h2>*Firebase Provides⬇️</h2>
+          <p><CheckCircleIcon height={20}/>Analytics</p>
+          <p><CheckCircleIcon height={20}/>Authentication </p>
+          <p><CheckCircleIcon height={20}/>Cloud messaging</p>
+          <p><CheckCircleIcon height={20}/>Realtime database</p>
+          <p><CheckCircleIcon height={20}/>Crashlytics <span className="ps-2 text-danger" >ETC.....</span> </p>
         </div>
       </div>
     </div>
